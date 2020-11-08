17405836c3c7432f72849cd07a1be2c78c581057
merima98 <merimaceranic98@gmail.com>
2020-11-08 15:55:15 +0100

    Adding conditional redirects

--- src/containers/Blog/NewPost/NewPost.js
+++ src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import axios from "axios";
+import { Redirect } from "react-router-dom";
 import "./NewPost.css";
 
 class NewPost extends Component {
@@ -7,6 +8,7 @@ class NewPost extends Component {
     title: "",
     content: "",
     author: "Merima",
+    submitted: false,
   };
 
   componentDidMount() {
@@ -20,12 +22,18 @@ class NewPost extends Component {
     };
     axios.post("/posts", post).then((response) => {
       console.log(response);
+      this.setState({ submitted: true });
     });
   };
 
   render() {
+    let redirect = null;
+    if (this.state.submitted) {
+      redirect = <Redirect to="/posts" />;
+    }
     return (
       <div className="NewPost">
+        {redirect}
         <h1>Add a Post</h1>
         <label>Title</label>
         <input
