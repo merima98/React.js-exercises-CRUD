de099229b40520e4e3de72ddbe2a81b395c6d3ab
merima98 <merimaceranic98@gmail.com>
2020-11-09 12:19:46 +0100

    Adding guards

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -4,6 +4,10 @@ import { Route, NavLink, Switch, Redirect } from "react-router-dom";
 import Posts from "./Posts/Posts";
 import NewPost from "./NewPost/NewPost";
 class Blog extends Component {
+  state = {
+    auth: false,
+  };
+
   render() {
     return (
       <div className="Blog">
@@ -39,7 +43,9 @@ class Blog extends Component {
         </header>
 
         <Switch>
-          <Route path="/new-post" component={NewPost} />
+          {this.state.auth ? (
+            <Route path="/new-post" component={NewPost} />
+          ) : null}
           <Route path="/posts" component={Posts} />
           <Redirect from="/" to="/posts" />
           {/* <Route path="/" component={Posts} /> */}
